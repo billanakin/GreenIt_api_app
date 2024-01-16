811901fbaf9cd9c68434157adf71852755d22240
Bill Anakin A. Zamora <bill.anakin143@gmail.com>
2024-01-16 16:12:31 +0800

    feat: ngrok aware

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  if ENV.fetch('NGROK_HOST').present?
+    config.hosts << ENV.fetch('NGROK_HOST')
+  end
 end
