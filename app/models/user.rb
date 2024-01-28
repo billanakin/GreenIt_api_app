595f0fcba58181bddec44dd344985d25d6de0d5f
Bill Anakin A. Zamora <bill.anakin143@gmail.com>
2024-01-29 07:07:44 +0800

    feat: user temporary avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  before_create :attach_temporary_avatar
+
+  private
+
+  def attach_temporary_avatar
+    return null if email.blank?
+
+    slug = email.gsub(/\W+/, '-').downcase
+    self.profile_pic_url = Faker::Avatar.image(slug: slug, size: "833x786")
+  end
 end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -2,3 +2,4 @@ json.id user.id
 json.email user.email
 json.first_name user.first_name
 json.last_name user.last_name
+json.avatar_url user.profile_pic_url
